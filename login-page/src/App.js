e497282e80209d949460bc6468979a8d6fa2455d
Avanthika Rajesh <avanthikamahe64@gmail.com>
2024-05-03 13:06:15 +0530

    login-page

---
+++ login-page/src/App.js
@@ -0,0 +1,12 @@
+import LoginRegister from "./components/Login-register/LoginRegister";
+
+
+function App() {
+  return (
+    <div>
+      <LoginRegister/>
+    </div>
+  );
+};
+
+export default App;
